a7f5c423facb43f5d92727f5f70f6cd78d202db4
steelydylan <ess_president@me.com>
2018-10-14 20:09:00 +0900

    added dev env

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import * as YouTubeIframeLoader from 'youtube-iframe';
+import rangesliderJs from 'rangeslider-js';
+
+YouTubeIframeLoader.load((YT) => {
+  const searchBtn = document.querySelector('.js-search-btn');
+  const search: HTMLInputElement = document.querySelector('.js-search');
+  const sliderWrap = document.querySelector('.js-slider');
+
+  searchBtn.addEventListener('click', () => {
+    const videoId = search.value;
+    const player = new YT.Player('player', {
+      height: '390',
+      width: '640',
+      videoId,
+      events: {
+        onReady: () => {
+          sliderWrap.innerHTML = '';
+          const slider = document.createElement('input');
+          slider.setAttribute('type', 'range');
+          sliderWrap.appendChild(slider);
+          player.playVideo();
+          const max = player.getDuration();
+          rangesliderJs.create(slider, {
+            min: 0, 
+            max, 
+            value: 0, 
+            step: 1,
+            onSlideEnd: (value) => {
+              player.seekTo(value);
+            }
+          });
+        }
+      }
+    });
+  });
+});
